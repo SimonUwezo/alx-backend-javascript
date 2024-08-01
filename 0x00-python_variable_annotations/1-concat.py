709f5805fe54062ed2075181d334abb328011102
SimonUwezo <simonnmwangi@gmail.com>
2024-08-01 03:40:12 -0700

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function that takes a string str1 and a string str2 as 
+arguments and returns a concatenated string.
+
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that concatenates 2 strings"""
+
+    return str1 + str2
